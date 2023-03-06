4fee3d21e13fe4cfc512ccede0a73c9db3a756e8
Kerny <kerny14@gmail.com>
2023-03-06 12:52:01 -0500

    commit tree 60758007e5a6f055426efff6dba944c3f89530af

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1');
+
+document.body.append(h1)
+
+document.get
+
+// var newId = document.getElementsByTagName('h1')
+// newId.setAttributes('id')
+// h1.id = 'victory'
+          
+        
+
